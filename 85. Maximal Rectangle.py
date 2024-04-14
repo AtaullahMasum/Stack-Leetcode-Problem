158147517d420fadba1a79723778f6a65b2a7440
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-04-14 18:29:56 +0600

    solution ii added

--- 85. Maximal Rectangle.py
+++ 85. Maximal Rectangle.py
@@ -37,3 +37,49 @@ class Solution:
                 max_area = max(max_area, (right[j] - left[j]) * height[j])
 
         return max_area
+# Solution 2 
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        stack = []
+        leftsmaller, rightsmaller = [0]*n, [0]*n
+        #calculate leftsmaller index 
+        for i in range(n):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if not stack:
+                leftsmaller[i] = 0
+            else:
+                leftsmaller[i] = stack[-1] + 1
+            stack.append(i)
+        # clear stack to be reused 
+        while stack:
+            stack.pop()
+        #Calculate Rightsamller index
+        for i in range(n-1, -1, -1):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if not stack:
+                rightsmaller[i] = n-1
+            else:
+                rightsmaller[i] = stack[-1] - 1
+        # MaxArea replace here so reduce one more iteration
+            stack.append(i)
+        # Calculate MaxArea in the Histogram
+        maxArea = 0
+        for i in range(n):
+            maxArea = max(maxArea, (rightsmaller[i] - leftsmaller[i] + 1)*heights[i])
+        return maxArea
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        n , m = len(matrix), len(matrix[0])
+        maxArea = 0
+        heights = [0]*m
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == '1':
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
+            area = self.largestRectangleArea( heights)
+            maxArea = max(maxArea, area)
+        return maxArea
